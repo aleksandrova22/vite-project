7b9f2a87fb400c849504fe6f10b6f10a0108f068
aleksandrova22 <lialia1986@mail.ru>
2025-01-25 22:48:26 +0700

    2501++

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,9 @@ export type ToDo = { id: number; text: string };
 
 export class ToDoItem {
   id = Math.random();
-  constructor(text: string | '') {
-    Object.assign(this, { text, id: Math.random() });
+  
+  constructor(text: string | '' ) {
+    Object.assign(this, { text , id: Math.random() } as ToDo);
   }
 }
 
@@ -15,7 +16,7 @@ export class ToDoItem {
 function App() {
   const [count, setCount] = useState(0),
     [value, setValue] = useState(''),
-    [list, setList] = useState([] as any),
+    [list, setList] = useState([] as ToDo[] ),
     delItem = (id: number) => { setList([...list].filter(item => item.id != id)) };
   return <>
     <h2>Vite + React</h2>
@@ -30,9 +31,9 @@ function App() {
       <legend>Todo</legend>
       <input value={value} onChange={event => setValue(event.target.value)} />
 
-      <button onClick={() => setList([...list, new ToDoItem(value)])}>➕add</button>
+      <button onClick={() => setList([...list, new ToDoItem(value) as ToDo])}>➕add</button>
       <ol>
-        {[...list].map(({ id, text }) => <li key={id}>
+        {[...list]?.map?.(({id, text}) => <li key={id}>
           {text}
           <button onClick={() => delItem(id)}>❌</button>
 
